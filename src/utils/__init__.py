bac3732d2c370842f66a4d94ed1400b22cf9d82a
Pawel Kaminski <pawel.kaminski@uni.lu>
2023-10-26 17:19:46 +0200

    Fix import formatting

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,2 +1,6 @@
 from utils.file_mgnt import create_book_subdir  # noqa
-from utils.read_config import BookConfigParser, BookMetadata, TableOfContents  # noqa
+from utils.read_config import (
+    BookConfigParser,
+    BookMetadata,
+    TableOfContents,
+)  # noqa
